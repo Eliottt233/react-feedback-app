79d9c2a2c325123a162f3ff7ffdee29928c0bc1e
Ziheng JING <ziheng.jing@student.adelaide.edu.au>
2022-08-28 23:35:57 +0930

    First Full Version

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import {useState} from 'react';
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import {v4 as uuidv4} from 'uuid';
+
+function App(){
+    const [feedback, setFeedback] = useState(FeedbackData);
+    const deleteFeedback = (id) => {
+        console.log(id);
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id));
+        }
+    }
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]);
+    }
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList 
+                feedback={feedback}
+                handleDelete={deleteFeedback}/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default App;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'My first feedback'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'My second feedback'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'My third feedback'
+    },
+];
+
+export default FeedbackData;
